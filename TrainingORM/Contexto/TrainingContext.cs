b32539732f20af4e8188d2f3b62907b4c6be0b09
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2022-08-07 19:02:32 -0300

    Alterado configuração de conexão com banco (EF)

--- TrainingORM/Contexto/TrainingContext.cs
+++ TrainingORM/Contexto/TrainingContext.cs
@@ -1,11 +1,27 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.Win32;
+using System.Data.Entity;
 using TrainingLibrary.Entity;
 
 namespace TrainingORM.Contexto
 {
-    [DbConfigurationType(typeof(TreiningDbConfiguration))]
     public class TrainingContext : DbContext
     {
+        private static string connectionString()
+        {
+            var registryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Training";
+            var registryKey = "ConnectionString";
+
+            var connectionStringObj = Registry.GetValue(registryPath, registryKey, null);
+            var connectionString = connectionStringObj as string ?? throw new Exception("Não foi possivel obter a chave da ConnectionString");
+
+            return connectionString;
+        }
+
+
+        public TrainingContext() : base(connectionString())
+        {
+        }
+
         public DbSet<Exercise> Exercise { get; set; }
         public DbSet<ExerciseSeries> ExerciseSeries { get; set; }
         public DbSet<Training> Training { get; set; }
