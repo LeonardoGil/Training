5d5df073ed50c3f59c88d063cd3f2d6915ee2063
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2022-08-07 15:04:06 -0300

    ORM

---
+++ TrainingORM/Contexto/TrainingContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using TrainingLibrary.Entity;
+
+namespace TrainingORM.Contexto
+{
+    public class TrainingContext : DbContext
+    {
+        public DbSet<Exercise> Exercise { get; set; }
+        public DbSet<ExerciseSeries> ExerciseSeries { get; set; }
+        public DbSet<Training> Training { get; set; }
+        public DbSet<TrainingExercise> TrainingExercise { get; set; }
+    }
+}
