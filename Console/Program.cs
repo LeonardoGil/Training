ffccf4ab74837856913eff7fbed1512409788962
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2022-08-07 21:21:31 -0300

    Ajuste no projeto Console

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,4 @@
-﻿using TrainingLibrary.Entity;
-using TrainingORM.Contexto;
+﻿using TrainingConsole.Test;
 
 namespace TrainingConsole
 {
@@ -8,62 +7,9 @@ namespace TrainingConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Process Training.exe!");
-            MockTest.On();
+            TestEF.Start();
 
             Console.WriteLine("Exit code 0.");
-        }
-    }
-
-    public class MockTest
-    {
-        public static void On()
-        {
-            using (var contexto = new TrainingContext())
-            {
-                var exercise = new Exercise()
-                {
-                    Id = Guid.NewGuid(),
-                    Description = "Rosca Direta",
-                    Observation = null
-                };
-
-                contexto.Exercise.Add(exercise);
-                contexto.SaveChanges();
-
-                var trainig = new Training()
-                {
-                    Id = Guid.NewGuid(),
-                    Date = new DateTime(2022, 07, 08),
-                    Duration = null,
-                    Exercises = new List<TrainingExercise>()
-                };
-
-                var trainigExercise = new TrainingExercise()
-                {
-                    Duration = new TimeSpan(1, 10, 0),
-                    ExerciseId = exercise.Id,
-                    Id = Guid.NewGuid(),
-                    Order = 1,
-                    Series = new List<ExerciseSeries>(),
-                    TrainingId = trainig.Id
-                };
-
-                var exerciseSerie = new ExerciseSeries()
-                {
-                    Id = Guid.NewGuid(),
-                    Order = 1,
-                    Reps = 10,
-                    RestTime = null,
-                    Weight = 14,
-                    TrainingExerciseId = trainigExercise.Id
-                };
-
-                trainigExercise.Series.Add(exerciseSerie);
-                trainig.Exercises.Add(trainigExercise);
-
-                contexto.Training.Add(trainig);
-                contexto.SaveChanges();
-            }
         }
     }
 }
