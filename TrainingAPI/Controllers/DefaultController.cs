cbf8baa2e38028b68b4d4ae9ae46d6d8f6da67bd
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2022-08-12 20:06:42 -0300

    API

---
+++ TrainingAPI/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace TrainingAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
