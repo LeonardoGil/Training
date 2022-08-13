611ec35ddd23854d28ea9c12d66de22e3d2dd0c4
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2022-08-13 16:46:08 -0300

    Incluindo Referencias ao projeto e adicionado Injetor de Dependencia

---
+++ TrainingService/Settings/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TrainingORM.Contexto;
+
+#region ORM
+using TrainingORM.Interface;
+using TrainingORM.Repository;
+#endregion
+
+#region Service
+using TrainingService.Interface;
+using TrainingService.Service;
+#endregion
+
+namespace TrainingService.Settings
+{
+    public static class DependencyInjection
+    {
+        public static void Inject(IServiceCollection services)
+        {
+            services.AddScoped<TrainingContext>();
+
+            services.InjectRepository();
+            services.InjectService();
+        }
+
+        private static void InjectRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IExerciseRepository, ExerciseRepository>();
+        }
+
+        private static void InjectService(this IServiceCollection services)
+        {
+            services.AddScoped<IExerciseService, ExerciseService>();
+        }
+    }
+}
