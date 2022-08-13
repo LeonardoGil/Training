d8b151923f884dda5593127d46ab6260093e121c
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2022-08-13 16:43:04 -0300

    Padrão do C#

--- Console/Test/TestEF.cs
+++ Console/Test/TestEF.cs
@@ -7,52 +7,50 @@ namespace TrainingConsole.Test
     {
         public static void Start()
         {
-            using (var contexto = new TrainingContext())
+            using var contexto = new TrainingContext();
+            var exercise = new Exercise()
             {
-                var exercise = new Exercise()
-                {
-                    Id = Guid.NewGuid(),
-                    Description = "Rosca Direta",
-                    Observation = null
-                };
+                Id = Guid.NewGuid(),
+                Description = "Rosca Direta",
+                Observation = null
+            };
 
-                contexto.Exercise.Add(exercise);
-                contexto.SaveChanges();
+            contexto.Exercise.Add(exercise);
+            contexto.SaveChanges();
 
-                var trainig = new Training()
-                {
-                    Id = Guid.NewGuid(),
-                    Date = new DateTime(2022, 07, 08),
-                    Duration = null,
-                    Exercises = new List<TrainingExercise>()
-                };
+            var trainig = new Training()
+            {
+                Id = Guid.NewGuid(),
+                Date = new DateTime(2022, 07, 08),
+                Duration = null,
+                Exercises = new List<TrainingExercise>()
+            };
 
-                var trainigExercise = new TrainingExercise()
-                {
-                    Duration = new TimeSpan(1, 10, 0),
-                    ExerciseId = exercise.Id,
-                    Id = Guid.NewGuid(),
-                    Order = 1,
-                    Series = new List<ExerciseSeries>(),
-                    TrainingId = trainig.Id
-                };
+            var trainigExercise = new TrainingExercise()
+            {
+                Duration = new TimeSpan(1, 10, 0),
+                ExerciseId = exercise.Id,
+                Id = Guid.NewGuid(),
+                Order = 1,
+                Series = new List<ExerciseSeries>(),
+                TrainingId = trainig.Id
+            };
 
-                var exerciseSerie = new ExerciseSeries()
-                {
-                    Id = Guid.NewGuid(),
-                    Order = 1,
-                    Reps = 10,
-                    RestTime = null,
-                    Weight = 14,
-                    TrainingExerciseId = trainigExercise.Id
-                };
+            var exerciseSerie = new ExerciseSeries()
+            {
+                Id = Guid.NewGuid(),
+                Order = 1,
+                Reps = 10,
+                RestTime = null,
+                Weight = 14,
+                TrainingExerciseId = trainigExercise.Id
+            };
 
-                trainigExercise.Series.Add(exerciseSerie);
-                trainig.Exercises.Add(trainigExercise);
+            trainigExercise.Series.Add(exerciseSerie);
+            trainig.Exercises.Add(trainigExercise);
 
-                contexto.Training.Add(trainig);
-                contexto.SaveChanges();
-            }
+            contexto.Training.Add(trainig);
+            contexto.SaveChanges();
         }
     }
 
